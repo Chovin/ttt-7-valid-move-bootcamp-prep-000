49f04c8abc0d4df4a4ce347c3de1236194fb24d6
Chovin Carlson <Chovin@users.noreply.github.com>
2018-08-31 01:19:10 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,9 @@
 # code your #valid_move? method here
 
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  if board[index].nil?
+    return false
+  end
+  board[index].count("XO") > 0
+end
